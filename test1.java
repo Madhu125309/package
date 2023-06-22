627ff84d3ada17be1bc8dc2d7649ec6e19b4d68a
7997259422 <madhureddy85767@gmail.com>
2023-06-22 15:01:10 +0530

    fun

---
+++ test1.java
@@ -0,0 +1,19 @@
+class account {
+    private double min_bal;
+
+    public double get_min_bal() {
+        return this.min_bal;
+    }
+
+    public void set_min_bal(double bal) {
+        this.min_bal = bal;
+    }
+}
+
+public class test1 {
+    public static void main(String[] args) {
+        account b = new account();
+        b.set_min_bal(100.00);
+        System.out.println(b.get_min_bal());
+    }
+}
